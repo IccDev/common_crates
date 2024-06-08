c5e8b94734ac5450fd0752c0b8814aa788a6d0da
djedou <djedouarnaud.aman@gmail.com>
2024-06-08 22:44:00 +0200

    settup

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+pub use tokio;
+pub use tracing;
+pub use tracing_subscriber;
+pub use warp;
+pub use acteur;
+pub use remoc;
+pub use sqlx;
+pub use async_trait;
+pub use serde;
+pub use serde_json;
+pub use serde_derive;
+pub use sea_orm;
+pub use async_std;
+pub use sea_orm_migration;
+
+/*
+pub use hyper;
+pub use futures;
+pub use match_request;
+pub use multipart;
+pub use mime;
+pub use uuid;
+pub use mailchecker;
+pub use zxcvbn;
+pub use chbs;
+pub use argon2;
+pub use time;
+pub use http_client;
+pub use url;
+pub use hyper_rustls;
+pub use rustls;
+pub use rustls_pemfile;
+pub use rayon;
+*/
